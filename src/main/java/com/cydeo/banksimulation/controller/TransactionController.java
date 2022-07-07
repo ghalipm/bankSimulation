4f7ccc0fbb5c7400c99c859cab01119c2d1a868a
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-07-06 22:22:24 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/banksimulation/controller/TransactionController.java
@@ -0,0 +1,64 @@
+package com.cydeo.banksimulation.controller;
+
+import com.cydeo.banksimulation.model.Account;
+import com.cydeo.banksimulation.model.Transaction;
+import com.cydeo.banksimulation.service.AccountService;
+import com.cydeo.banksimulation.service.TransactionService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.UUID;
+
+@Controller
+public class TransactionController {
+
+    private final AccountService accountService;
+    private final TransactionService transactionService;
+
+    public TransactionController(AccountService accountService, TransactionService transactionService) {
+        this.accountService = accountService;
+        this.transactionService = transactionService;
+    }
+
+
+    @GetMapping("/make-transfer")
+    public String retrieveTransactionForm(Model model){
+        model.addAttribute("accounts", accountService.listAllAccount());
+        model.addAttribute("transaction", Transaction.builder().build());
+        model.addAttribute("lastTransactionList", transactionService.retrieveLastTransaction());
+
+        return "transaction/make-transfer";
+
+    }
+
+
+    @PostMapping("/transfer")
+    public String makeTransfer(@Valid @ModelAttribute("transaction")Transaction transaction, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("accounts", accountService.listAllAccount());
+            return "transaction/make-transfer";
+        }
+
+        Account reciever = accountService.retriveById(transaction.getReceiver());
+        Account sender = accountService.retriveById(transaction.getSender());
+        transactionService.makeTransfer(transaction.getAmount(),new Date(),sender,reciever,transaction.getMessage());
+        return "redirect:/make-transfer";
+
+    }
+
+    @GetMapping("/transaction/{id}")
+    public String transactionDetailById(@PathVariable("id")UUID id, Model model) {
+
+        model.addAttribute("transactionList", transactionService.findTransactionListById(id));
+
+        return "transaction/transactions";
+    }
+}
---
+++ src/main/java/com/cydeo/banksimulation/service/AccountService.java
@@ -0,0 +1,20 @@
+package com.cydeo.banksimulation.service;
+
+import com.cydeo.banksimulation.model.Account;
+import com.cydeo.banksimulation.enums.AccountType;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public interface AccountService {
+
+    Account createNewAccount(BigDecimal balance, Date creationDate, AccountType accountType, Long userId);
+
+    List<Account> listAllAccount();
+
+    void deleteAccount(UUID account);
+
+    Account retriveById(UUID account);
+}
